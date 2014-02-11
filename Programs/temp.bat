2c777fa3cc0311e419a9873baa81e495fb36711a
Nigel Cleland <nigel.cleland@gmail.com>
2014-02-11 14:41:47 +1300

    Generated parameters

--- Programs/temp.bat
+++ Programs/temp.bat
@@ -4,5 +4,5 @@ if exist runVSPDSetupProgress.txt       erase runVSPDSetupProgress.txt /q
 if exist runVSPDSolveProgress.txt       erase runVSPDSolveProgress.txt /q
 if exist runVSPDMergeProgress.txt       erase runVSPDMergeProgress.txt /q
 if exist runVSPDReportProgress.txt      erase runVSPDReportProgress.txt /q
-if exist "Z:\home\nigel\python\vSPD\Programs\..\Output\Test"        rmdir "Z:\home\nigel\python\vSPD\Programs\..\Output\Test" /s /q
-mkdir "Z:\home\nigel\python\vSPD\Programs\..\Output\Test"
+if exist "Z:\home\nigel\vSPD\Programs\..\Output\Test"        rmdir "Z:\home\nigel\vSPD\Programs\..\Output\Test" /s /q
+mkdir "Z:\home\nigel\vSPD\Programs\..\Output\Test"
